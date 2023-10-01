c893b97784345a745745521ef8b5279d0fc66428
gurshaagrawal1211 <gurshaagrawal@gmail.com>
2023-10-01 20:14:22 +0530

    commit

--- app.js
+++ app.js
@@ -52,7 +52,84 @@ const products = {
       price:"399",
     },
   ],
-    
+  MAKEUP:[
+    {
+      id:0,
+      img:"./images/makeup1.jpg",
+      desc:"Red Matt Lipstick",
+      price:"599",
+    },
+    {
+      id:1,
+      img:"./images/makeup2.jpg",
+      desc:"Makeup Brush Set",
+      price:"499",
+    },
+    {
+      id:2,
+      img:"./images/makeup3.jpg",
+      desc:"Illuminating face cream",
+      price:"999",
+    },
+    {
+      id:3,
+      img:"./images/makeup4.jpg",
+      desc:"Conclear",
+      price:"399",
+    },
+  ],
+  BAGS:[
+    {
+      id:0,
+      img:"./images/bag1.jpg",
+      desc:"Baby pink Hand-held Bag",
+      price:"899",
+    },
+    {
+      id:1,
+      img:"./images/bag2.jpg",
+      desc:"Multicolor Shoulder Bag",
+      price:"699",
+    },
+    {
+      id:2,
+      img:"./images/bag3.jpg",
+      desc:"Red Shoulder Bag",
+      price:"799",
+    },
+    {
+      id:3,
+      img:"./images/bag4.jpg",
+      desc:"Sea green Hand-held Bag",
+      price:"999",
+    },
+  ],
+   FOOTWEAR:[
+    {
+      id:0,
+      img:"./images/footwear1.jpg",
+      desc:"Casual Sneakers",
+      price:"399",
+    },
+    {
+      id:1,
+      img:"./images/footwear2.jpg",
+      desc:"Flower Print Heels",
+      price:"699",
+    },
+    {
+      id:2,
+      img:"./images/footwear3.jpg",
+      desc:"Boots For Women",
+      price:"899",
+    },
+    {
+      id:3,
+      img:"./images/footwear4.jpg",
+      desc:"Baby Pink Shoes",
+      price:"499",
+    },
+  ],
 };
 
 
@@ -75,6 +152,41 @@ const currentProductPrice=document.querySelector(".price");
 window.addEventListener("load",()=>{
     wrapper.style.transform="translate(0vw)"
   
+    productsToDisplay=products.CLOTHES;
+
+
+        productsToDisplay.map((pro)=>{
+
+          
+          const productItem=document.createElement('div');
+          productItem.classList.add('product');
+
+          const productImg=document.createElement('div');
+          productImg.classList.add('productImg');
+
+          productImg.innerHTML=`<img src="${pro.img}" alt="${pro.name}" class="productImg-sm">`
+
+          const productDetails=document.createElement('div');
+          productDetails.classList.add('productDetails');
+          
+          productDetails.innerHTML=`
+            <span class="desc">${pro.desc}</span>
+            <span class="price">₹${pro.price}</span>
+          `;
+
+          const btns=document.createElement('div');
+          btns.classList.add('btns');
+
+          btns.innerHTML=`
+            <button class="cartBtn">Add to Cart</button>
+            <button class="buyBtn">Buy Now</button>
+          `;
+
+        productDetails.appendChild(btns);
+        productItem.appendChild(productImg);
+        productItem.appendChild(productDetails);
+        productsContainer.appendChild(productItem);
+        })
 
 })
 
